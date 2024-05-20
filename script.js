adc64481674ed325d828f78bd0fef4a7896e214f
Alex1612356356 <84451489+Alex1612356356@users.noreply.github.com>
2024-05-20 19:24:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-function validate() {
-    if (document.contactForm.fName.value=="") {
-        alert("Enter Your First Name");
+const searchIcon = document.getElementById('searchIcon');
+const searchDropdown = document.getElementById('searchDropdown');
+
+searchIcon.addEventListener('click', function() {
+    if (searchDropdown.style.display === 'none' || searchDropdown.style.display === '') {
+        searchDropdown.style.display = 'block';
+    } else {
+        searchDropdown.style.display = 'none';
     }
-    else if (document.contactForm.lName.value=="") {
-        alert("Enter Your Last Name");
-    }
-    else if (document.contactForm.email.value=="") {
-        alert("Enter Your Email");
-    }
-    else if (document.contactForm.description.value=="") {
-        alert("Enter Your Description");
-    }
-    else{
-        alert("Message sent successfully!");
-    }
-}
+});
